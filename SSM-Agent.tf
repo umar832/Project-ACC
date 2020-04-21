73891b63dcb5c0306cb84b5eef7a5b8e2faea835
umar832 <59777479+umar832@users.noreply.github.com>
2020-04-21 11:35:27 -0500

    Create SSM-Agent.tf

---
+++ SSM-Agent.tf
@@ -0,0 +1,14 @@
+---
+- hosts: all
+  become: yes
+  become_method: sudo
+  tasks:
+  - name: install amazon-ssm-agent
+    yum:
+      name: https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+      state: present
+
+  - name: start amazon ssm-agent
+    systemd:
+      state: restarted
+      name: amazon-ssm-agent
